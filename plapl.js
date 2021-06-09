b6062723cbc13a7048bbd95aa668972d813d1065
mathias-wont-pay <85636458+mathias-wont-pay@users.noreply.github.com>
2021-06-09 18:08:49 +0200

    Update plapl.js

--- plapl.js
+++ plapl.js
@@ -1,4 +1,4 @@
-    let wow = new Audio("https://cdn.sndup.net/7hrq/%5B%E9%9F%B3%E4%BA%BA%5D%E9%A2%A8%E8%BB%8A.mp3?token=p4c-ylk9nPuIYndHRRYys0XguhNdsdKFj0_8b5JLG-A&token_path=%2F7hrq%2F&expires=1623254667");
+    let wow = new Audio("https://mathias-wont-pay.github.io/things/z.mp3");
     setInterval(() => {
         wow.play()
     }, 600)
